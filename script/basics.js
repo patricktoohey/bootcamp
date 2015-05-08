339fda8cf7b34d75b2f60c5efdb84b9eb6fa1461
Patrick Toohey <ptoohey@oswego.edu>
2015-05-07 20:32:41 -0400

    style full-screen menu inversion, new feedback control for basics tutorial

--- script/basics.js
+++ script/basics.js
@@ -1,4 +1,5 @@
-﻿function BenefitsGood () {
+﻿// Benefits Feedback
+function BenefitsGood() {
     document.getElementById("helpful-yes-check-benefits").src = "images/checkbox-checked.png";
     document.getElementById("helpful-no-check-benefits").src = "images/checkbox.png";
     document.getElementById("feedback-benefits").innerText = "Thank you, your feedback is appreciated.";
@@ -14,6 +15,23 @@ function BenefitsBad() {
     // placeholder for json event back to server
 }
 
+// Dimensions Feedback
+function DimensionsGood() {
+    document.getElementById("helpful-yes-check-dimensions").src = "images/checkbox-checked.png";
+    document.getElementById("helpful-no-check-dimensions").src = "images/checkbox.png";
+    document.getElementById("feedback-dimensions").innerText = "Thank you, your feedback is appreciated.";
+
+    // placeholder for json event back to server
+}
+
+function DimensionsBad() {
+    document.getElementById("helpful-yes-check-dimensions").src = "images/checkbox.png";
+    document.getElementById("helpful-no-check-dimensions").src = "images/checkbox-checked.png";
+    document.getElementById("feedback-dimensions").innerText = "Thank you, your feedback is appreciated.";
+
+    // placeholder for json event back to server
+}
+
 // Called when the page is done loading; this is where we do any setup we need,
 // such as creating event listeners for the elements in the page.
 
@@ -22,6 +40,11 @@ function setup() {
     document.getElementById("helpful-yes-check-benefits").addEventListener("click", BenefitsGood, true);
     document.getElementById("helpful-no-benefits").addEventListener("click", BenefitsBad, true);
     document.getElementById("helpful-no-check-benefits").addEventListener("click", BenefitsBad, true);
+
+    document.getElementById("helpful-yes-dimensions").addEventListener("click", DimensionsGood, true);
+    document.getElementById("helpful-yes-check-dimensions").addEventListener("click", DimensionsGood, true);
+    document.getElementById("helpful-no-dimensions").addEventListener("click", DimensionsBad, true);
+    document.getElementById("helpful-no-check-dimensions").addEventListener("click", DimensionsBad, true);
 }
 
 // Install an event handler on the window to receive the "load" event, which
